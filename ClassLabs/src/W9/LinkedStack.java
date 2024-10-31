8dee42f7321da3f0a21e726f0a87ee9472a116ac
300368842 <300368842@N5111S32.ad.douglascollege.ca>
2024-10-31 12:17:21 -0700

    W9

--- ClassLabs/src/W9/LinkedStack.java
+++ ClassLabs/src/W9/LinkedStack.java
@@ -1,10 +1,28 @@
 package W9;
 
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Queue;
+import java.util.Vector;
+
 public class LinkedStack {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
+		
+		Vector<Integer> vec = new Vector<Integer>();
+		vec.add(45);
+		vec.add(90); //push
+		
+		System.out.println(vec);
+		vec.remove(vec.size()-1); //pop method
+		vec.add(990); //push
+		vec.add(6868); //push
+		System.out.println(vec);
+		vec.get(vec.size()-1); // peek
+		
+		
 	}
 
 }
